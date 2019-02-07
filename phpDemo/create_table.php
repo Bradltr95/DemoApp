fb1303d4879b74fc0cca075a91037d94cec51044
Bradltr95 <47356514+Bradltr95@users.noreply.github.com>
2019-02-07 15:19:04 -0500

    Update create_table.php
    Took away the 'lastname' entry into our 'userdb' as it was not needed in the project.

--- phpDemo/create_table.php
+++ phpDemo/create_table.php
@@ -6,7 +6,6 @@ require_once "app/connect.php";
 $sql = "CREATE TABLE users (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
 username VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL
 )";
 // attempt the query to our database 
 if($conn->query($sql) == TRUE) {

793c049b1c49cd55cf7b153a28f8788ebd60a7f9
Bradltr95 <47356514+Bradltr95@users.noreply.github.com>
2019-02-08 18:41:28 -0500

    Update create_db.php
    Included file description and cleaned up some of the code.

--- phpDemo/create_db.php
+++ phpDemo/create_db.php
@@ -1,24 +1,30 @@
+<!-- create_db.php - a file used to create a database that will work for 
+our application. This way anyone that wants to use our project doesn't have to 
+create the database and data table themselves. 
+@database name: 'userdb' 
+@database table name: 'users' 
+--> 
 <?php 
-// connect to the db 
+// database variables 
 $servername = "localhost"; 
 $username = "root"; 
 $password = ""; 
 
-// create connection 
+// create new connection using the database variables 
 $conn = new mysqli($servername, $username, $password) or die("Unable to create connection"); 
-
-// create the users database
-// database name: 'userdb' 
-// table name: 'users' 
 
 // store our query command into $sql 
 $sql = "CREATE DATABASE userdb"; 
-// attempt the query to out database 
+
+// check to see if the query was successful to our data_table. 
 if($conn->query($sql) == TRUE) {
+	// let the user know we have delete the database
 	echo "'userdb' Database created successfully"; 
 } else {
+	// report any errors back to the user 
 	echo "Error creating database: " . $conn->error; 
 }
 ?> 
+<!-- provide a link back to the home page --> 
 <br /><br />  
 <a href="index.php">Home Page</a> 

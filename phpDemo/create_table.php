58eb6da24133c25e7c03a2f5470e29ca5a0e880e
Bradltr95 <47356514+Bradltr95@users.noreply.github.com>
2019-02-08 18:44:18 -0500

    Update create_table.php
    Added a description for the file and cleaned up some of the code.

--- phpDemo/create_table.php
+++ phpDemo/create_table.php
@@ -1,3 +1,6 @@
+<!-- create_table.php - a file used to create the database table for our project 
+so that the user does not have to do it manually themselves. We can use $conn->error 
+to let the user know if the table was already created --> 
 <?php 
 // connect to the db 
 require_once "app/connect.php"; 
@@ -7,12 +10,16 @@ $sql = "CREATE TABLE users (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
 username VARCHAR(30) NOT NULL,
 )";
+
 // attempt the query to our database 
 if($conn->query($sql) == TRUE) {
+	// let the user know if the table was created 
 	echo "'Users' Table created successfully";
 } else {
+	// report any errors back to the user
 	echo "Error creating table: " . $conn->error; 
 }
 ?> 
+<!-- provide a link back to the home page --> 
 <br /><br />  
 <a href="index.php">Home Page</a> 

1bf7bd47ac6032017c7ec3d8518bd1f20579829a
Bradltr95 <47356514+Bradltr95@users.noreply.github.com>
2019-02-08 20:28:21 -0500

    Update connect.php
    Cleaned up the code in the connect.php file.

--- phpDemo/app/connect.php
+++ phpDemo/app/connect.php
@@ -1,13 +1,13 @@
 <?php
-// Php file that connects to the database for us so we dont have to
-// hardcode connect everytime we want to submit a query
+/* connect.php - A PHP file that contains the necessary code to connect to our userdb so 
+we do not have to hardcode connect everytime we want to interact with our database. */
 
 /* database/server variables */
 $servername = "localhost"; 
 $username = "root"; 
 $password = ""; 
 $db = "userdb"; 
+
 // store connection in $conn variable. 
 $conn = new mysqli($servername, $username, $password, $db) or die("Unable to connect");
-
 ?>
